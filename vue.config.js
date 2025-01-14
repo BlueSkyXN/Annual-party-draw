b127476a9e4f284904b6b0fb83e30dda55415e5a
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2025-01-14 14:19:27 +0800

    0.1.0

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,6 @@
 module.exports = {
   publicPath: '/',
-  devServer: {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'text/javascript'
-    }
-  },
-  chainWebpack: config => {
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap(options => ({
-        ...options,
-        compilerOptions: {
-          // 任何特殊编译器选项
-        }
-      }));
-  }
+  outputDir: 'dist',
+  assetsDir: '',
+  productionSourceMap: false
 };
